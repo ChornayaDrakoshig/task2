f31b1f9a17ac99d6bee9992b56941b3ff2cb3206
Hannah Bugai <ganna.bugai@yandex.ru>
2017-10-10 17:00:15 +0300

    1-basic task1 v2

--- 1-Basic/js/task1.js
+++ 1-Basic/js/task1.js
@@ -2,18 +2,21 @@ function showCurrentTime() {
 var dayoptions = {
         weekday: 'long'
     };
-var timeoptions = {
-    timezone: 'UTC',
-    hour: 'numeric',
-    hour12: true,
-    minute: 'numeric',
-    second: 'numeric'
-};
 
 var dayWrapper = document.getElementById('weekday');
 var timeWrapper = document.getElementById('time');
 var date = new Date();
-
+var hour = date.getHours();
+var minute = date.getMinutes();
+var sec = date.getSeconds();
+    
 dayWrapper.innerHTML = date.toLocaleString('en-US', dayoptions);
-timeWrapper.innerHTML = date.toLocaleString('en-US', timeoptions);
+var output = hour>12 ? ""+(hour-12)+" PM" : ""+hour+" AM";
+if (hour === 0) output="12 Midnight"
+    else if (hour === 12) output="12 Noon";
+output+=" : ";
+output+= minute>9 ? minute : "0"+minute;
+output+=" : ";
+output+= sec>9 ? sec : "0"+sec;
+timeWrapper.innerHTML = output;
 }
