fe8fa3cf6f6fef37cd383a9b0b1121c22fbf2a51
Hannah Bugai <ganna.bugai@yandex.ru>
2017-10-12 14:44:49 +0300

    task15

---
+++ 1-Basic/js/task15.js
@@ -0,0 +1,10 @@
+var answ = prompt("Write the number", '');
+if (answ != null){
+    if (answ > 13) {
+        alert((answ-13)*2);
+    } 
+    else {
+        alert(13-answ);
+    }
+    alert(str);
+}
