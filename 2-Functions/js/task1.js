cd12e6e00e63e2c182b94f6a7fe6c79a6663cdc4
Hannah Bugai <ganna.bugai@yandex.ru>
2017-10-13 19:58:01 +0300

    task1

--- 2-Functions/js/task1.js
+++ 2-Functions/js/task1.js
@@ -1,6 +1,11 @@
 function reverse(num){
-    var str = num.toString;
-    return str.split('').reverse().join(''); // str.split is not a function ???
+    var str = num.toString();
+    var l = str.length;
+    var str2 = "";
+    for (i=l-1;i>=0;i--){
+        str2+=str[i];
+    }
+    return str2;
 }
 
 console.log("23444 - "+reverse(23444));
