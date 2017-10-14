bfc8ba9a19665016d2e5750b629696e8abdbffe0
Hannah Bugai <ganna.bugai@yandex.ru>
2017-10-14 16:44:46 +0300

    task12

---
+++ 2-Functions/js/task12.js
@@ -0,0 +1,14 @@
+function isPerfect(n){
+    var sum=1;
+    for (i=2;i<Math.floor(n/2)+1;i++){
+
+        if (n % i === 0) sum+=i;
+                console.log(i+" "+sum);
+    }
+    return (n===sum);
+}
+
+console.log("6 "+isPerfect(6));
+console.log("28 "+isPerfect(28));
+console.log("33 "+isPerfect(33));
+//console.log("496 "+isPerfect(496));
