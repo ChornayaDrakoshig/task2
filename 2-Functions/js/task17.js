22e2799de7ac43c0cb9e6fd505327912af67a3ee
Hannah Bugai <ganna.bugai@yandex.ru>
2017-10-14 18:55:34 +0300

    task17

--- 2-Functions/js/task17.js
+++ 2-Functions/js/task17.js
@@ -1,16 +1,14 @@
 function unique(srt){
     var arr=srt.toLowerCase().split('').sort();
-    console.log(arr);
     var i=0, j=0;
     while (i<arr.length){
         while (arr[i] == arr[i+j+1]){
             j++;
         }
-        if (j>0) arr.splice(i+1, j+1);
+        if (j>0) arr.splice(i+1, j);
         j=0;
         i++;
     }
-    console.log(arr);
     return arr.join('');
 }
 
@@ -21,19 +19,12 @@ function occurrences(srt){
     for (k=0;k<arr2.length;k++){
         answ.push([arr2[k],0]);
     }
-    console.log(answ);
-    console.log(answ[0][1]);
-    var j=1;
-    var k=0;
-    console.log(arr.length);
+    var j=1, k=0;
     for (i=0;i<arr.length;i++){
-        console.log(i+" "+j+" "+k)
         while (arr[i] == arr[i+j] && i+j<arr.length){
             j++;
         }
-        
         answ[k][1]=j;
-        console.log(answ[k]);
         i+=j-1;
         j=1;
         k++;
@@ -41,6 +32,5 @@ function occurrences(srt){
     return answ;
 }
 
-//console.log(occurrences( "thequickbrownfoxjumpsoverthelazydog"));
-console.log(occurrences( "brownfoxjumplazydog"));
-//console.log(occurrences('aaaaaaaabbaa'));
+console.log(occurrences( "thequickbrownfoxjumpsoverthelazydog"));
+console.log(occurrences('aaaaaaaabbaac'));
