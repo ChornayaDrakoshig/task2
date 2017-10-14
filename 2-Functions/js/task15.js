43f6a8e83bc17f1f521bf522e2c9472e82a60040
Hannah Bugai <ganna.bugai@yandex.ru>
2017-10-14 17:39:29 +0300

    task15

---
+++ 2-Functions/js/task15.js
@@ -0,0 +1,8 @@
+function compExp(b,n){
+    return Math.pow(b,n);
+}
+
+console.log("2^3="+compExp(2,3));
+console.log("3^2="+compExp(3,2));
+console.log("16^0.5="+compExp(16,0.5));
+console.log("2^-1="+compExp(2,-1));
