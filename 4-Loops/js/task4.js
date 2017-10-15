f3e869f5dc1c0ee14c2729f11885c26c51e3feed
Hannah Bugai <ganna.bugai@yandex.ru>
2017-10-15 13:34:29 +0300

    task4

---
+++ 4-Loops/js/task4.js
@@ -0,0 +1,9 @@
+function largeNum(a,b,c,d,e){
+    if (a>b && a>c && a>d && a>e) alert(a)
+    else if (b>a && b>c && b>d && b>e) alert(b)
+    else if (c>a && c>b && c>d && c>e) alert(c)
+    else if (d>a && d>b && d>c && d>e) alert(d)
+    else alert(e);
+}
+
+largeNum( -5, -2, -6, 0, -1);
