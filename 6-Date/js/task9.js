0b7423f7ef0bce8971ad7162137f0f850eaf67c2
Hannah Bugai <ganna.bugai@yandex.ru>
2017-10-17 19:58:42 +0300

    task9

---
+++ 6-Date/js/task9.js
@@ -0,0 +1,6 @@
+var lastday = function(year,month) {
+ return new Date(year, month+1, 0).getDate();
+};
+console.log(lastday(2014,0)); 
+console.log(lastday(2014,1)); 
+console.log(lastday(2014,11));
